01ed6445bddf9c9a231e982107a6fe3576d7e4b0
Braden Wiggins <braden@fractal-hq.com>
2024-06-15 21:30:57 -0400

    feat: add site

---
+++ www/src/lib/components/Header/Nav/Nav.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { setContext } from 'svelte'
+	import { mobile } from 'fractils'
+
+	import Mobile from './Mobile/Mobile.svelte'
+	import Desktop from './Desktop.svelte'
+
+	setContext('links', [
+		['/elements', 'Elements'],
+		['/about', 'About'],
+		['/contact', 'Contact'],
+	])
+</script>
+
+<template lang="pug">
+
+	+if('$mobile')
+
+		Mobile
+
+		+else
+			Desktop
+
+</template>
