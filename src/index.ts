3910fe2d491df29b4140e8b957ee314e1863b5e8
Braden Wiggins <braden@fractal-hq.com>
2024-06-15 22:07:21 -0400

    feat: exports

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export type { TransformOptions } from './core/transform'
+export { Transform } from './core/transform'
+
+export type { CameraOptions } from './core/camera'
+export { Camera } from './core/camera'
+
+export type { Vec2, Vec3 } from './core/vectors'
+export { Vector3 } from './core/vectors'
+
+export type { UniformOptions, UniformValueType } from './core/uniform'
+export { Uniform } from './core/uniform'
+
+export type { Mat4 } from './core/matrix'
+export { Matrix4 } from './core/matrix'
+
+export type { GeometryOptions } from './core/geometry'
+export { Geometry } from './core/geometry'
+
+export type { StageOptions } from './core/stage'
+export { Stage } from './core/stage'
+
+export {
+	type QuerySelector,
+	subtractVectors,
+	cross_arr,
+	normalize,
+	degToRad,
+	radToDeg,
+	select,
+	cross,
+} from './core/utils'
