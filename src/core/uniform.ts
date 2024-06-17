50692cc64f9c96b3cf7e2d8ca86528c7461a2d36
Braden Wiggins <braden@fractal-hq.com>
2024-06-17 17:08:17 -0400

    feat: matrix uniforms / warn on missing uniforms

--- src/core/uniform.ts
+++ src/core/uniform.ts
@@ -6,6 +6,9 @@ export type UniformValueType =
 	| { type: 'vec2'; value: (...args: any[]) => [number, number] }
 	| { type: 'vec3'; value: (...args: any[]) => [number, number, number] }
 	| { type: 'vec4'; value: (...args: any[]) => [number, number, number, number] }
+	| { type: 'mat2'; value: (...args: any[]) => Float32Array | number[] }
+	| { type: 'mat3'; value: (...args: any[]) => Float32Array | number[] }
+	| { type: 'mat4'; value: (...args: any[]) => Float32Array | number[] }
 
 export type UniformOptions = {
 	name: string
@@ -15,21 +18,30 @@ export type UniformOptions = {
 export class Uniform {
 	name: string
 	location: WebGLUniformLocation
-	value: UniformValueType['value']
 	type: UniformValueType['type']
 
+	value: UniformValueType['value']
 	update: (...args: any[]) => Uniform
 
-	constructor(public gl: WebGL2RenderingContext, public program: WebGLProgram, options: UniformOptions) {
+	constructor(
+		public gl: WebGL2RenderingContext,
+		public program: WebGLProgram,
+		options: UniformOptions,
+	) {
 		this.name = options.name
 		this.type = options.type
 		this.value = options.value
+
 		this.location = gl.getUniformLocation(program, options.name)!
-		if (!this.location) {
-			console.error('Failed to get uniform location', { name: options.name, location: this.location })
-			throw new Error('Failed to get uniform location')
+		if (!this.location && this.location !== 0) {
+			console.warn(
+				`Unused uniform detected - %c${this.name}`,
+				'color: #f88; font-weight: bold;',
+				'\nEnsure it is used in the shader program, or remove it from the uniform list.',
+			)
 		}
 		this.update = () => {
+			if (!this.location) return this
 			options.update(this.location, this.value())
 			return this
 		}
