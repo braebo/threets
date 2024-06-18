fc2935b1fd5263cbb79ce88fb3d160df34ce83cc
Braden Wiggins <braden@fractal-hq.com>
2024-06-17 23:07:35 -0400

    feat: basic scene

--- www/src/lib/scenes/basic.ts
+++ www/src/lib/scenes/basic.ts
@@ -2,35 +2,51 @@ import { Stage } from 'threets'
 
 export async function basicScene() {
 	const stage = new Stage({
-		vertex: `#version 300 es
-precision mediump float;
+// 		vertex: `#version 300 es
+// precision mediump float;
 
-in vec4 a_position;
-uniform vec2 u_resolution;
-uniform mat4 u_matrix;
-out vec2 v_uv;
+// in vec4 a_position;
+// uniform vec2 u_resolution;
+// uniform mat4 u_matrix;
+// out vec2 v_uv;
 
-void main() {
-    v_uv = a_position.xy / u_resolution;
-    gl_Position = u_matrix * a_position;
+// void main() {
+//     v_uv = a_position.xy * 0.5 + 0.5;
+//     gl_Position = a_position;
+// }`,
+// 		fragment: `#version 300 es
+// precision mediump float;
 
-    gl_PointSize = 2.0;
-}`,
-		fragment: `#version 300 es
-precision mediump float;
+// in vec2 v_uv;
+// out vec4 fragColor;
 
-in vec2 v_uv;
-out vec4 fragColor;
-
-void main() {
-   fragColor = vec4(v_uv, v_uv.y + v_uv.x, 1.0);
-}`,
+// void main() {
+//    fragColor = vec4(v_uv.x, v_uv.y, v_uv.y + v_uv.x, 1.0);
+// }`,
 	})
 
+
+
+	// // prettier-ignore
 	// stage.addGeometry({
-	// 	name: 'a_grid',
-	// 	positions: new Float32Array([-1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1]),
+	// 	name: 'a_position',
+	// 	positions: new Float32Array([
+    //         -1, -1, 0,  // bottom left corner
+    //          1, -1, 0,  // bottom right corner
+    //         -1,  1, 0,  // top left corner
+    //          1,  1, 0,  // top right corner
+    //     ]),
+    //     indices: new Uint16Array([0, 1, 2, 2, 1, 3]),
 	// })
+    stage.addSimpleQuad()
+
+	// After your WebGL calls
+	let error = stage.gl?.getError()
+	if (error != stage.gl?.NO_ERROR) {
+		console.error('WebGL Error: ' + error)
+	}
 
 	stage.render()
+
+	return stage
 }
--- www/src/routes/playground/basic/+page.svelte
+++ www/src/routes/playground/basic/+page.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
-	import { basicScene } from '$lib/scenes/basic'
-	import { onMount } from 'svelte'
+	import type { Stage } from 'threets'
 
-	onMount(basicScene)
+	import { basicScene } from '$lib/scenes/basic'
+	import { onDestroy, onMount } from 'svelte'
+
+	let stage: Stage
+
+	onMount(async () => {
+		stage = await basicScene()
+	})
+
+	onDestroy(() => {
+		globalThis.window?.location.reload()
+	})
 </script>
 
-wip
+<button on:click={() => console.log(stage)}>log(stage)</button>
+<button on:click={() => console.log((stage.canvas.width = stage?.canvas.width - 1))}>wat</button>
 
 <style lang="scss">
+	button {
+		margin: 0.5rem auto;
+		width: 10rem;
+	}
+
+	:global(#app canvas) {
+		width: 90%;
+		border-radius: 1rem;
+		margin: 1rem auto;
+	}
 </style>
