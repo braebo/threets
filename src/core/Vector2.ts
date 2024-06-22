d9dd4805ab9a4364b1e618ad416817f309b7ef11
Braden Wiggins <braden@fractal-hq.com>
2024-06-22 19:02:48 -0400

    chore: vector rework

--- src/core/Vector2.ts
+++ src/core/Vector2.ts
@@ -1,66 +1,253 @@
-import { isNumber, Log } from './utils'
+import { LinkedListener } from './Listener'
+import {
+	isNumber,
+	// Log
+} from './utils'
 
 export type Vec2 = { x: number; y: number }
-export type Vector2Like = Partial<Vec2> | [number, number] | number
+export type Vector2Like = number | Partial<Vec2> | Vector2
 
 export function isVector2(thing: any): thing is Vector2 {
 	return thing && thing.type === 'Vector2'
 }
 
-@Log('Vector2', ['get', 'toJSON'])
+// @Log('Vector2', ['get', 'toJSON'])
 export class Vector2 {
-	readonly __type = 'Vector2' as const
-	_x = 0
-	_y = 0
+	readonly isVector2 = true as const
+	private _x = 0
+	private _y = 0
 
-	constructor(xOrVec2Like?: Vector2Like, y?: number) {
-		xOrVec2Like ??= 0
-		this.set(xOrVec2Like, y)
+	constructor(xOrVec2?: number | Partial<Vec2> | Vector2, y?: number) {
+		xOrVec2 ??= 0
+		if (isNumber(xOrVec2)) {
+			this._x = xOrVec2
+			this._y = y ?? this._x
+		} else {
+			this._x = xOrVec2.x ?? 0
+			this._y = xOrVec2.y ?? this._x
+		}
 	}
 
 	get x() {
 		return this._x
 	}
+	set x(value: number) {
+		this._x = value
+		this._emit()
+	}
+
 	get y() {
 		return this._y
 	}
-
-	get(): Vec2 {
-		return { x: this.x, y: this.y }
-	}
-	set(xOrVec2Like: Vector2Like, y?: number): this {
-		if (Array.isArray(xOrVec2Like)) {
-			this._x = xOrVec2Like[0]
-			this._y = xOrVec2Like[1]
-		} else if (typeof xOrVec2Like === 'object') {
-			if (isNumber(xOrVec2Like.x)) this._x = xOrVec2Like.x
-			if (isNumber(xOrVec2Like.y)) this._y = xOrVec2Like.y
-		} else if (typeof xOrVec2Like === 'string') {
-			if (xOrVec2Like === 'x') this._x = y!
-			if (xOrVec2Like === 'y') this._y = y!
-		} else {
-			this._x = xOrVec2Like ?? 1
-			this._y = y ?? this.x
-		}
-		return this
-	}
-
-	setX(value: number): this {
-		this._x = value
-		return this
-	}
-	setY(value: number): this {
+	set y(value: number) {
 		this._y = value
+		this._emit()
+	}
+
+	/**
+	 * Returns this vector as a new plain `{x,y}` {@link Vec2} object.
+	 */
+	get(): Vec2 {
+		return this.toJSON()
+	}
+	/**
+	 * Sets the values of this vector.
+	 */
+	set(vec2: Partial<Vec2>): this {
+		this._x = vec2.x ?? this._x
+		this._y = vec2.y ?? this._y
+		this._emit()
 		return this
 	}
 
-	toArray(): [number, number] {
-		return [this.x, this.y]
+	/**
+	 * Copies the provided vector's values onto this vector.
+	 */
+	copy(vec: Vector2): this {
+		this._x = vec._x
+		this._y = vec._y
+		this._emit()
+		return this
 	}
-	toObject(): Vec2 {
-		return this.get()
+
+	/**
+	 * Sets the values of this vector's {@link x} and {@link y} components.
+	 */
+	setXY(x: number, y: number): this {
+		this._x = x
+		this._y = y
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Updates this vector by adding `inputVector`.
+	 */
+	add(vec: Vector2): Vector2 {
+		this._x += vec._x
+		this._y += vec._y
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Updates this vector by subtracting `inputVector`.
+	 */
+	subtract(inputVector: Vector2): Vector2 {
+		this._x -= inputVector._x
+		this._y -= inputVector._y
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Updates this vector by multiplying `inputVector`.
+	 */
+	multiply(vec: Vector2): Vector2 {
+		this._x *= vec._x
+		this._y *= vec._y
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Updates this vector by multiplying by `scalar`.
+	 */
+	multiplyScalar(scalar: number): Vector2 {
+		this._x *= scalar
+		this._y *= scalar
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Updates this vector by dividing by `inputVector`.
+	 */
+	divide(vec: Vector2): Vector2 {
+		this._x /= vec._x
+		this._y /= vec._y
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Calculates the dot product with another vector.
+	 */
+	dot(v: Vector2): number {
+		return this._x * v._x + this._y * v._y
+	}
+
+	/**
+	 * Normalizes this vector (makes it a unit vector).
+	 */
+	normalize(): this {
+		const length = Math.sqrt(this._x * this._x + this._y * this._y)
+		if (length > 0) {
+			this._x /= length
+			this._y /= length
+		}
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Calculates the length of this vector, returning the result.
+	 */
+	length(): number {
+		return Math.sqrt(this._x * this._x + this._y * this._y)
+	}
+
+	/**
+	 * An array representation of this vector.
+	 */
+	toArray(): [number, number] {
+		return [this._x, this._y]
 	}
 	toJSON() {
-		return this.get()
+		return { x: this._x, y: this._y }
+	}
+
+	/**
+	 * Takes a callback to run whenever {@link x} or {@link y} change.  Calling this
+	 * method multiple times will result in multiple callbacks being run.
+	 */
+	onChange(callback: (vector: Vector2) => void) {
+		this._listeners ??= new LinkedListener(callback)
+		this._listeners.add(callback)
+	}
+	private _listeners: LinkedListener<[Vector2]> | null = null
+	private _next: LinkedListener<[Vector2]> | null = null
+	private _emit() {
+		this._next = this._listeners
+		while (this._next) {
+			this._next = this._next.emit(this)
+		}
 	}
 }
+
+// import { isNumber, Log } from './utils'
+
+// export type Vec2 = { x: number; y: number }
+// export type Vector2Like = Partial<Vec2> | [number, number] | number
+
+// export function isVector2(thing: any): thing is Vector2 {
+// 	return thing && thing.type === 'Vector2'
+// }
+
+// // @Log('Vector2', ['get', 'toJSON'])
+// export class Vector2 {
+// 	readonly __type = 'Vector2' as const
+// 	_x = 0
+// 	_y = 0
+
+// 	constructor(xOrVec2Like?: Vector2Like, y?: number) {
+// 		xOrVec2Like ??= 0
+// 		this.set(xOrVec2Like, y)
+// 	}
+
+// 	get x() {
+// 		return this._x
+// 	}
+// 	get y() {
+// 		return this._y
+// 	}
+
+// 	get(): Vec2 {
+// 		return { x: this.x, y: this.y }
+// 	}
+// 	set(xOrVec2Like: Vector2Like, y?: number): this {
+// 		if (Array.isArray(xOrVec2Like)) {
+// 			this._x = xOrVec2Like[0]
+// 			this._y = xOrVec2Like[1]
+// 		} else if (typeof xOrVec2Like === 'object') {
+// 			if (isNumber(xOrVec2Like.x)) this._x = xOrVec2Like.x
+// 			if (isNumber(xOrVec2Like.y)) this._y = xOrVec2Like.y
+// 		} else if (typeof xOrVec2Like === 'string') {
+// 			if (xOrVec2Like === 'x') this._x = y!
+// 			if (xOrVec2Like === 'y') this._y = y!
+// 		} else {
+// 			this._x = xOrVec2Like ?? 1
+// 			this._y = y ?? this.x
+// 		}
+// 		return this
+// 	}
+
+// 	setX(value: number): this {
+// 		this._x = value
+// 		return this
+// 	}
+// 	setY(value: number): this {
+// 		this._y = value
+// 		return this
+// 	}
+
+// 	toArray(): [number, number] {
+// 		return [this.x, this.y]
+// 	}
+// 	toObject(): Vec2 {
+// 		return this.get()
+// 	}
+// 	toJSON() {
+// 		return this.get()
+// 	}
+// }
--- src/core/Vector3.ts
+++ src/core/Vector3.ts
@@ -1,73 +1,70 @@
-import { isNumber, Log } from './utils'
+import { LinkedListener } from './Listener'
+import {
+	isNumber,
+	// Log
+} from './utils'
 
 export type Vec3 = { x: number; y: number; z: number }
-export type Vector3Like = Partial<Vec3> | [number, number, number] | number
+export type Vector3Like = number | Partial<Vec3> | Vector3
 
 export function isVector3(thing: any): thing is Vector3 {
 	return thing && thing.type === 'Vector3'
 }
 
-@Log('Vector3', ['get', 'toJSON'])
+// @Log('Vector3', ['get', 'toJSON'])
 export class Vector3 {
-	readonly __type = 'Vector3' as const
-	_x = 0
-	_y = 0
-	_z = 0
+	readonly isVector3 = true as const
+	private _x = 0
+	private _y = 0
+	private _z = 0
 
-	constructor(xOrVec3Like?: Vector3Like, y?: number, z?: number) {
-		xOrVec3Like ??= 0
-		this.set(xOrVec3Like ?? 0, y, z)
+	constructor(xOrVec3?: Vector3Like, y?: number, z?: number) {
+		xOrVec3 ??= 0
+		if (isNumber(xOrVec3)) {
+			this._x = xOrVec3
+			this._y = y ?? this._x
+			this._z = z ?? this._y
+		} else {
+			this._x = xOrVec3.x ?? 0
+			this._y = xOrVec3.y ?? this._x
+			this._z = xOrVec3.z ?? this._y
+		}
 	}
 
 	get x() {
 		return this._x
 	}
+	set x(value: number) {
+		this._x = value
+	}
+
 	get y() {
 		return this._y
 	}
+	set y(value: number) {
+		this._y = value
+	}
+
 	get z() {
 		return this._z
 	}
+	set z(value: number) {
+		this._z = value
+	}
 
 	/**
-	 * Returns this vector as a new plain {@link Vec3} object.
+	 * Returns this vector as a new plain `{x,y,z}` {@link Vec3} object.
 	 */
 	get(): Vec3 {
 		return this.toJSON()
 	}
 	/**
-	 * Sets the values of this vector from the provided {@link Vector3Like}, which can be:
-	 * - #### ( x, y?, z? )
-	 *   - If just `x` is provided, `y` and `z` will fallback to `x`.
-	 * - #### { x?, y?, z? }
-	 *   - This is a partial {@link Vec3}, and only the provided properties will be set.
-	 * - #### [ x, y, z ]
-	 *   - All 3 values must be provided in an array.
-	 * - #### {@link Vector3}
-	 *   - Passing a whole vector is a less efficient than just using {@link Vector3.copy}
+	 * Sets the values of this vector.
 	 */
-	set(xOrVec3Like: Vector3Like, y?: number, z?: number): this {
-		if (Array.isArray(xOrVec3Like)) {
-			this._x = xOrVec3Like[0]
-			this._y = xOrVec3Like[1]
-			this._z = xOrVec3Like[2]
-		} else if (typeof xOrVec3Like === 'object') {
-			if (isVector3(xOrVec3Like)) {
-				this.copy(xOrVec3Like)
-			} else {
-				if (isNumber(xOrVec3Like.x)) this._x = xOrVec3Like.x
-				if (isNumber(xOrVec3Like.y)) this._y = xOrVec3Like.y
-				if (isNumber(xOrVec3Like.z)) this._z = xOrVec3Like.z
-			}
-		} else if (typeof xOrVec3Like === 'string') {
-			if (xOrVec3Like === 'x') this._x = y!
-			if (xOrVec3Like === 'y') this._y = y!
-			if (xOrVec3Like === 'z') this._z
-		} else {
-			this._x = xOrVec3Like ?? 1
-			this._y = y ?? this.x
-			this._z = z ?? this.y ?? this.x
-		}
+	set(vec3: Partial<Vec3>): this {
+		this._x = vec3.x ?? this._x
+		this._y = vec3.y ?? this._y
+		this._z = vec3.z ?? this._z
 		return this
 	}
 
@@ -75,33 +72,19 @@ export class Vector3 {
 	 * Copies the provided vector's values onto this vector.
 	 */
 	copy(vec: Vector3): this {
-		this._x = vec.x
-		this._y = vec.y
-		this._z = vec.z
+		this._x = vec._x
+		this._y = vec._y
+		this._z = vec._z
 		return this
 	}
 
 	/**
-	 * Fastest way to set {@link x}.
+	 * Sets the values of this vector's {@link x}, {@link y}, and {@link z} components.
 	 */
-	setX(value: number): this {
-		this._x = value
-		return this
-	}
-
-	/**
-	 * Fastest way to set {@link y}.
-	 */
-	setY(value: number): this {
-		this._y = value
-		return this
-	}
-
-	/**
-	 * Fastest way to set {@link z}.
-	 */
-	setZ(value: number): this {
-		this._z = value
+	setXYZ(x: number, y: number, z: number): this {
+		this._x = x
+		this._y = y
+		this._z = z
 		return this
 	}
 
@@ -109,46 +92,131 @@ export class Vector3 {
 	 * Updates this vector by adding `inputVector`.
 	 */
 	add(vec: Vector3): Vector3 {
-		this._x += vec.x
-		this._y += vec.y
-		this._z += vec.z
+		this._x += vec._x
+		this._y += vec._y
+		this._z += vec._z
 		return this
 	}
+
 	/**
 	 * Updates this vector by subtracting `inputVector`.
 	 */
 	subtract(inputVector: Vector3): Vector3 {
-		this._x -= inputVector.x
-		this._y -= inputVector.y
-		this._z -= inputVector.z
+		this._x -= inputVector._x
+		this._y -= inputVector._y
+		this._z -= inputVector._z
 		return this
 	}
+
 	/**
 	 * Updates this vector by multiplying `inputVector`.
 	 */
 	multiply(vec: Vector3): Vector3 {
-		this._x *= vec.x
-		this._y *= vec.y
-		this._z *= vec.z
+		this._x *= vec._x
+		this._y *= vec._y
+		this._z *= vec._z
 		return this
 	}
+
+	/**
+	 * Updates this vector by multiplying by `scalar`.
+	 */
+	multiplyScalar(scalar: number): Vector3 {
+		this._x *= scalar
+		this._y *= scalar
+		this._z *= scalar
+		return this
+	}
+
 	/**
 	 * Updates this vector by dividing by `inputVector`.
 	 */
 	divide(vec: Vector3): Vector3 {
-		this._x /= vec.x
-		this._y /= vec.y
-		this._z /= vec.z
+		this._x /= vec._x
+		this._y /= vec._y
+		this._z /= vec._z
 		return this
+	}
+
+	/**
+	 * Calculates the dot product with another vector.
+	 */
+	dot(v: Vector3): number {
+		return this._x * v._x + this._y * v._y + this._z * v._z
+	}
+
+	/**
+	 * Calculates the cross product with another vector and stores the result in this vector.
+	 */
+	cross(v: Vector3): this {
+		const ax = this._x,
+			ay = this._y,
+			az = this._z
+		const bx = v._x,
+			by = v._y,
+			bz = v._z
+
+		this._x = ay * bz - az * by
+		this._y = az * bx - ax * bz
+		this._z = ax * by - ay * bx
+
+		return this
+	}
+
+	/**
+	 * Normalizes this vector (makes it a unit vector).
+	 */
+	normalize(): this {
+		const length = Math.sqrt(this._x * this._x + this._y * this._y + this._z * this._z)
+		if (length > 0) {
+			this._x /= length
+			this._y /= length
+			this._z /= length
+		}
+		return this
+	}
+
+	/**
+	 * Calculates the length of this vector, returning the result.
+	 */
+	length(): number {
+		return Math.sqrt(this._x * this._x + this._y * this._y + this._z * this._z)
 	}
 
 	/**
 	 * An array representation of this vector.
 	 */
 	toArray(): [number, number, number] {
-		return [this.x, this.y, this.z]
+		return [this._x, this._y, this._z]
 	}
-	toJSON() {
-		return { x: this.x, y: this.y, z: this.z }
+	toJSON(): Vec3 {
+		return { x: this._x, y: this._y, z: this._z }
+	}
+
+	/**
+	 * Notifies all {@link subscribe|subscribers}.
+	 */
+	update(): this {
+		this._emit()
+		return this
+	}
+
+	/**
+	 * Takes a callback to run whenever {@link x}, {@link y}, or {@link z} change.  Calling this
+	 * method multiple times will result in multiple callbacks being run.
+	 * @returns An unsubscribe function.
+	 */
+	subscribe(callback: (vector: Vector3) => void): () => void {
+		this._listeners ??= new LinkedListener(callback)
+		this._listeners.add(callback)
+		return this._listeners.disconnect
+	}
+	private _listeners: LinkedListener<Vector3> | null = null
+	private _next: LinkedListener<Vector3> | null = null
+	private _emit() {
+		this._next = this._listeners
+		while (this._next) {
+			this._next = this._next.emit(this)
+		}
 	}
 }
